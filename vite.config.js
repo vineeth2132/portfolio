969d53c8a8d4373a748742d97fcaff8e7417e14c
vineeth2132 <vineeth2320@gmail.com>
2025-08-05 19:31:17 +0530

    Clean reinstall to fix Vercel build

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/portfolio/', // ✅ important for GitHub Pages to resolve assets correctly
   plugins: [react()],
-  server: {
-    host: "0.0.0.0", // Expose to all network interfaces
-    port: 5173,       // Default port
-  },
 });
