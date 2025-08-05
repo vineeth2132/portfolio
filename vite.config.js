79fc139220fc40bf98e3cb0e62e31e36d732487e
vineeth2132 <vineeth2320@gmail.com>
2025-08-05 19:40:18 +0530

    to fix Vercel build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/portfolio/', // ✅ important for GitHub Pages to resolve assets correctly
+  base: '', // ✅ important for GitHub Pages to resolve assets correctly
   plugins: [react()],
 });
